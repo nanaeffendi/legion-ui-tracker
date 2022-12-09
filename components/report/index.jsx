ac0a56bd3fbee24e5ad6bdaa2e54836e3ac24377
nanaeffendi <ratnapuri.effendi@gmail.com>
2022-12-09 12:04:20 +0700

    update repo list

--- components/report/index.jsx
+++ components/report/index.jsx
@@ -2,7 +2,9 @@ import React, {useRef, useEffect} from "react";
 import {Runtime, Inspector} from "@observablehq/runtime";
 // import notebook from "d40c61bb9c87565a";
 // import notebook from "fa932951ede1a176";
-import notebook from "05ab94bd828086d1";
+// import notebook from "05ab94bd828086d1";
+import notebook from "80a93ab569acf736";
+
 
 
 function Notebook() {
@@ -17,7 +19,7 @@ function Notebook() {
   return (
     <div className="report-page--wrapper">
       <div ref={ref} />
-      <p>Credit: <a href="https://observablehq.com/d/05ab94bd828086d1">Legion-Site Component Tracker by nanaeffendi</a></p>
+      <p>Credit: <a href="https://observablehq.com/d/80a93ab569acf736">Legion-Site Component Tracker by nanaeffendi</a></p>
     </div>
   );
 }
